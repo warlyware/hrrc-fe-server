cf8c089fe8d997450056fc011a8f8f8541c9792a
warlyware <danward@gmail.com>
2017-04-23 17:55:21 -0400

    Update body copy

--- src/app/body-copy/body-copy.constant.js
+++ src/app/body-copy/body-copy.constant.js
@@ -11,10 +11,10 @@
                 'missionStatement': 'At the Human Rights Realization Community we envision a world where human rights are the cornerstone of a student’s education.'
             },
             'submissions': {
-                'main': 'In the hopes of creating a truly collaborative space for sharing knowledge about human rights, we will be posting blogs, articles, and papers written by the Human Rights Realization Community.  This section is dedicated to the community.  Feel free to browse through the contributions and share them with your friends and family.  Everyone is free to make submissions.  To contribute to the growing collection send your submissions to:'
+                'main': 'In the hopes of creating a truly collaborative space for sharing knowledge about human rights, we will be posting blogs, articles, papers, and research assignments written by the Human Rights Realization Community. This section is dedicated to the community. Feel free to browse through the contributions and share them with your friends and family. Everyone is free to make submissions. To contribute to the growing collection please send your submissions to:'
             },
             'teachingResources': {
-                'main': 'The resources found here are a result of years of human rights study and countless hours of research.  Through this lecture series, you can prepare human rights lectures and courses for your students.  The lectures cover the foundations of human rights beginning with the Cyrus Cylinder, Enlightenment thinkers, the League of Nations, the United Nations, and the work of its specialized agencies.  As our institution grows, we will continue to update this section with revisions and new content.  The lectures currently found here were originally utilized as part of the Human Rights courses offered in the Humanities Department at Florida State University.'
+                'main': 'The resources found here are a result of years of human rights study and countless hours of research. Through this series of lectures, lesson plans, and supplemental materials, you can incorporate human rights into your classes or use the information as a basis for class discussions or assignments. The lectures and lesson plans cover various topics related to human rights. As our community grows, we will continue to update this section with revisions and new content.'
             }
         });
 })();
